a7fca4ab6413963ddcec82f7d5b35445f7698124
Daniel <daniel.h.nunes@aluno.senai.br>
2026-06-11 16:33:55 -0300

    aprendendo sobre variaveis

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,55 @@ console.log("Comando de saída")
 
 //CRIANDO VARIÁVEIS
 
+let num = 50
+var num2 = 180
+const num3 = 500
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true){
+    let num4 = 420
+    var num5 = 650
+
+    console.log(num4)
+}
+
+console.log(num5)
+
+num = 2
+
+console.log(num)
+
+//CONCATENAÇÃO
+
+console.log('Valor de num =', num)
+console.log("Valor de num " + num)
+console.log('Valor de num2 ', num2)
+console.log(`Valor de num2 ${num2}`)
+
+console.log('A soma dos números ' + 15 + 8)
+console.log('A soma dos números ', 15 + 8)
+console.log(`A soma dos números ${15 + 8}`)
+
+console.log('A soma dos números ' + num + num2)
+console.log('A soma dos números ', num + num2)
+console.log(`A soma dos números ${num3 - num2}`)
+
+console.log('SOMA DAS VARIÁVEIS ', num + num2)
+console.log('SUBTRAÇÃO DAS VARIÁVEL ', num3 - num)
+console.log('MULTIPLICAÇÃO ', num5 * 2)
+console.log('DIVISÃO DE VALORES ', num2 / 5)
+console.log('RESTO DA DIVISÃO ', num2 % 2)
+console.log(`RESTO DA DIVISÃO ${num3 % 3}`)
+
+console.log('TESTE DE COMPARAÇÃO', 10 == 2)
+console.log('TESTE RELACIONAIS', 10 != 2)
+console.log('TESTE RELACIONAL', 10 > 2)
+console.log('TESTE RELACIONAL', 10 < 2)
+console.log('TESTE RELACIONAL', 10 >= 2)
+console.log('TESTE RELACIONAL', 10 <= 2)
+console.log('TESTE LÓGICO', (10 > 2) && (15 < 5))
+console.log('TESTE LÓGICO', (10 > 2) || (15 < 5))
+
